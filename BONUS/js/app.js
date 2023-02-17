5960baed4bdecd2f158fc0a8e00951a4da21c470
Lapo0 <lapo.gini.01@gmail.com>
2023-02-17 13:17:55 +0100

    REFACTOR BONUS in other html-flie

--- BONUS/js/app.js
+++ BONUS/js/app.js
@@ -5,9 +5,10 @@ const { createApp } = Vue
         data() {
             return {
                 titol: 'un messaggio',
+                img: 'https://www.tuttogreen.it/wp-content/uploads/2017/10/shutterstock_518591449-700x467.jpg'
             }
         }
     }
-
+    
 const app = createApp(option)
 app.mount('#app')

d2f45749d4b3881f5271f85032e8d20e03539b8b
Lapo0 <lapo.gini.01@gmail.com>
2023-02-17 13:15:12 +0100

    FEAT BONUS img

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 const { createApp } = Vue
-    console.log(Vue)
 
     // const createApp = Vue.createApp
     const option = {
         data() {
             return {
-                message: 'ciao',
+                titol: 'un messaggio',
+                img: 'https://www.tuttogreen.it/wp-content/uploads/2017/10/shutterstock_518591449-700x467.jpg'
             }
         }
     }
+    
 const app = createApp(option)
 app.mount('#app')

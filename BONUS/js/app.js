ebaf03ddea23bc617127f05b50fa96cb978efc4c
Lapo0 <lapo.gini.01@gmail.com>
2023-02-17 15:48:08 +0100

    FETA select

--- BONUS/js/app.js
+++ BONUS/js/app.js
@@ -4,10 +4,16 @@ const { createApp } = Vue
     const option = {
         data() {
             return {
-                titol: 'un messaggio',
-                img: 'https://www.tuttogreen.it/wp-content/uploads/2017/10/shutterstock_518591449-700x467.jpg'
+                titol: 'Scegli un numero da 1 a 8',
+                img: 'https://www.tuttogreen.it/wp-content/uploads/2017/10/shutterstock_518591449-700x467.jpg',
+                text: 'ciao',
             }
-        }
+        },
+        methods: {
+            saluta: function () {
+                console.log('Buongiorno')
+            },
+        },
     }
     
 const app = createApp(option)
